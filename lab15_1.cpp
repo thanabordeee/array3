e669032d5e87d9d8f0a099f310a1e5508a225989
thanabordeee <jejekup00123@hotamil.co.th>
2023-01-17 16:47:46 +0700

    complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[],int N)
+{
+int x;
+T numb;
 
+for(int i = 1; i <= N-1; i++)
+	{
+	numb = d[i];
+	for (x=i-1; x>=0&&numb>d[x]; x--)
+	{
+	 d[x+1] = d[x];
+	}
+	d[x+1] = numb;
+	cout << "Pass " << i << ":";
+	for (int y = 0; y < N; y++)
+      {
+        cout << d[y] << " ";
+      }
+cout <<endl;
 }
 
+}
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,51 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double A[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Row "<< i+1 << ": ";
+		for (int j = 0; j < N; j++)
+		{
+			cin >> A[i][j];
+			
+		}
+		
+	}
+	
+	
+
+}
+void findLocalMax(const double A[][N], bool B[][N])
+{
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+	{
+		if (i == 0 || j == 0 || i == N-1 || j == N-1)
+		{
+		  B[i][j] = {false};
+		}
+		else if(A[i][j] >= A[i-1][j] && A[i][j] >=A[i+1][j] && A[i][j]>= A[i][j+1] && A[i][j] >=A[i][j-1]) B[i][j] = {true};
+		else  B[i][j] = {false};
+	}
+	
+  }
+  
+}
+void showMatrix(const bool B[][N])
+{
+   
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << B[i][j]<< " ";
+		}
+		cout << endl;
+	}
+	
+
+  
+}
